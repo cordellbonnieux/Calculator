c9d0537ab2cbd9de8e965d5f0361439cc19a3191
cordellbonnieux <cordellbonnieux@gmail.com>
2020-07-29 21:37:42 -0700

    basic structure of calculator is built, along with all the button selectors.

---
+++ calc-script.js
@@ -0,0 +1,32 @@
+// display
+let display = document.getElementById("display");
+//buttons
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let six = document.getElementById("six");
+let seven = document.getElementById("seven");
+let eight = document.getElementById("eight");
+let nine = document.getElementById("nine");
+let zero = document.getElementById("zero");
+let divide = document.getElementById("divide");
+let multiply = document.getElementById("multiply");
+let subtract = document.getElementById("subtract");
+let add = document.getElementById("add");
+let equals = document.getElementById("equals");
+let clear = document.getElementById("clear");
+// math functions
+function addition(a, b){
+    return a + b;
+}
+function subtraction(a, b){
+    return a - b;
+}
+function multiplication(a, b){
+    return a * b;
+}
+function division(a, b){
+    return a / b;
+}
